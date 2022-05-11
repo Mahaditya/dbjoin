08256e51375d0ddbef86edcf8eaf9686c1a4c154
Mahaditya Kaushik <33066858+Mahaditya@users.noreply.github.com>
2022-05-11 11:13:29 +0530

    Adds a test for left Join array

--- src/lib/leftJoinArray.spec.ts
+++ src/lib/leftJoinArray.spec.ts
@@ -48,3 +48,64 @@ test('Left Join Array', (t) => {
   ]
   t.deepEqual(realValue,expectedValue)
 });
+
+
+
+test('Left Join Array - Multi Join', (t) => {
+  const realValue = leftJoinArray(
+    [
+      {
+        a: '1',
+        b: '2',
+        c: '3',
+      },
+      {
+        a: '2',
+        b: '3',
+        c: '4',
+      },
+    ],
+    [
+      {
+        a: '1',
+        g: '5',
+        h: '6',
+      },
+      {
+        a: '2',
+        g: '7',
+        h: '8',
+      },
+      {
+        a: '2',
+        g: '9',
+        h: '10',
+      },
+    ],['a']
+  );
+
+  const expectedValue = [
+    {
+      a: '1',
+      b: '2',
+      c: '3',
+      g: '5',
+      h: '6',
+    },
+    {
+      a: '2',
+      b: '3',
+      c: '4',
+      g: '7',
+      h: '8',
+    },
+    {
+      a: '2',
+      b: '3',
+      c: '4',
+      g: '9',
+      h: '10',
+    },
+  ]
+  t.deepEqual(realValue,expectedValue)
+});
