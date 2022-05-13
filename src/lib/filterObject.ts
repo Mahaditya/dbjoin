375956a360dbf8001c2db19dce47a10d030b7d42
Mahaditya Kaushik <33066858+Mahaditya@users.noreply.github.com>
2022-05-13 08:02:15 +0530

    Fix: filterObject type defs now support Interfaces

--- src/lib/filterObject.ts
+++ src/lib/filterObject.ts
@@ -1,12 +1,21 @@
 /* eslint-disable functional/no-let */
 /* eslint-disable functional/no-loop-statement */
-import type { RecordKeys } from '../domain';
+import type {
+  Columns,
+  RecordKeys,
+  Row,
+  Values,
+} from '../domain';
 
-export const filterObject = <A extends RecordKeys, B, C extends A, D>(
-  template: Record<C, B>,
-  obj: Record<A, D>
+export const filterObject = <
+  TemplateKeys extends Columns<Obj>,
+  TemplateValues extends RecordKeys,
+  Obj
+>(
+  template: Record<TemplateKeys, TemplateValues>,
+  obj: Row<Obj>
 ) => {
-  let filteredObject = {} as Record<C, D>;
+  let filteredObject = {} as Record<TemplateKeys, Values<Obj>>;
   for (const key in template) {
     filteredObject = { ...filteredObject, [key]: obj[key] };
   }
--- src/lib/filterObjectArray.ts
+++ src/lib/filterObjectArray.ts
@@ -1,8 +1,16 @@
-import type { RecordArray, RecordKeys } from '../domain';
+import type {
+  Columns,
+  RecordKeys,
+  Rows,
+} from '../domain';
 
 import { filterObject } from './filterObject';
 
-export const filterObjectArray = <A extends RecordKeys, B, C extends A, D>(
-  template: Record<C, B>,
-  objArray: RecordArray<A, D>
+export const filterObjectArray = <
+  TemplateKeys extends Columns<Obj>,
+  TemplateValues extends RecordKeys,
+  Obj
+>(
+  template: Record<TemplateKeys, TemplateValues>,
+  objArray: Rows<Obj>
 ) => objArray.map((obj) => filterObject(template, obj));
